608404f7bdbeda711cd44a633839204bb4190d11
Roman Nergachev <nergachev.roman@gmail.com>
2017-06-06 22:13:23 +0300

    feat: Model class for Flickr response

---
+++ app/src/main/java/com/rnergachev/imagegallery/data/model/FlickrResponse.java
@@ -0,0 +1,17 @@
+package com.rnergachev.imagegallery.data.model;
+
+import java.util.List;
+
+/**
+ * Model class for Flickr response
+ *
+ * Created by roman on 06.06.2017.
+ */
+
+public class FlickrResponse {
+    private List<FlickrImageData> items;
+
+    public List<FlickrImageData> getPhotos() {
+        return items;
+    }
+}

15a355d69cf366cbf204618271440fb4400a4fe9
Roman Nergachev <nergachev.roman@gmail.com>
2017-06-06 23:19:49 +0300

    feat: Add application component for dagger

---
+++ app/src/main/java/com/rnergachev/imagegallery/di/ApplicationComponent.java
@@ -0,0 +1,27 @@
+package com.rnergachev.imagegallery.di;
+
+
+import com.rnergachev.imagegallery.ui.activity.ImageActivity;
+import com.rnergachev.imagegallery.ui.adapter.ImagesOverviewAdapter;
+import com.rnergachev.imagegallery.ui.fragment.DetailImageFragment;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Application component for dagger
+ *
+ * Created by rnergachev on 06/06/2017.
+ */
+
+
+
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+    // inject context and retrofit service
+    void inject(ImagesOverviewAdapter imagesOverviewAdapter);
+    void inject(ImageActivity imageActivity);
+    void inject(DetailImageFragment detailImageFragment);
+}

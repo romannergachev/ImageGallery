123025b213fb566b0d5c20a6adac65b199a60a5c
Roman Nergachev <nergachev.roman@gmail.com>
2017-06-07 17:52:06 +0300

    feat: Provide corresponding view for the presenter and add activity layout

---
+++ app/src/main/java/com/rnergachev/imagegallery/ui/view/ImageActivityView.java
@@ -0,0 +1,12 @@
+package com.rnergachev.imagegallery.ui.view;
+
+/**
+ * View contract for ImageActivity
+ *
+ * Created by rnergachev on 06/06/2017.
+ */
+
+public interface ImageActivityView {
+
+    void notifyDataChanged();
+}

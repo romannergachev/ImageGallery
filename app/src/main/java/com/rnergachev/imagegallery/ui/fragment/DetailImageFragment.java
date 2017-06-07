a95a2e91cfc088631e440bf0991c89a442222c0f
Roman Nergachev <nergachev.roman@gmail.com>
2017-06-07 23:18:38 +0300

    feat: Fill metadata in fragment

--- app/src/main/java/com/rnergachev/imagegallery/ui/fragment/DetailImageFragment.java
+++ app/src/main/java/com/rnergachev/imagegallery/ui/fragment/DetailImageFragment.java
@@ -1,7 +1,7 @@
 package com.rnergachev.imagegallery.ui.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +14,10 @@ import com.rnergachev.imagegallery.R;
 import com.rnergachev.imagegallery.data.model.FlickrImageData;
 import com.rnergachev.imagegallery.ui.presenter.ImageActivityPresenter;
 import com.squareup.picasso.Picasso;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 import javax.inject.Inject;
 
@@ -37,8 +41,9 @@ public class DetailImageFragment extends Fragment {
         return fragment;
     }
 
-    @BindView(R.id.meta_text_view) TextView title;
+    @BindView(R.id.author_text_view) TextView author;
     @BindView(R.id.flickr_image_image_view) ImageView image;
+    @BindView(R.id.date_text_view) TextView date;
 
 
     private FlickrImageData flickrImageData;
@@ -62,6 +67,14 @@ public class DetailImageFragment extends Fragment {
 
         //show data on screen
         Picasso.with(getContext()).load(flickrImageData.getMedia().getImageLink()).into(image);
+
+        author.setText(flickrImageData.getAuthor());
+
+        DateTimeFormatter fmt = ISODateTimeFormat.dateTimeNoMillis();
+
+        DateTime dt = fmt.parseDateTime(flickrImageData.getDate());
+
+        date.setText(dt.toLocalDate().toString());
 
         if(position == imageActivityPresenter.getCurrentPosition()) {
             image.setTransitionName(getString(R.string.transition_image));
